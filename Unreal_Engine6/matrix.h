9a0193269d40a74cb0dad5376a6da1e340624d39
mronzon <104356392+mronzon@users.noreply.github.com>
2022-09-25 23:39:10 -0400

    Finish rotation matrix

--- Unreal_Engine6/matrix.h
+++ Unreal_Engine6/matrix.h
@@ -71,7 +71,30 @@ public:
                 result._matrix[7] = sin(angle);
                 result._matrix[8] = cos(angle);
                 break;
-        
+
+            case 1: // axis y
+                result._matrix[0] = cos(angle);
+                result._matrix[1] = 0;
+                result._matrix[2] = sin(angle);
+                result._matrix[3] = 0;
+                result._matrix[4] = 1;
+                result._matrix[5] = 0;
+                result._matrix[6] = -sin(angle);
+                result._matrix[7] = 0;
+                result._matrix[8] = cos(angle);
+                break;
+
+            case 2: // axis z
+                result._matrix[0] = cos(angle);
+                result._matrix[1] = -sin(angle);
+                result._matrix[2] = 0;
+                result._matrix[3] = sin(angle);
+                result._matrix[4] = cos(angle);
+                result._matrix[5] = 0;
+                result._matrix[6] = 0;
+                result._matrix[7] = 0;
+                result._matrix[8] = 1;
+                break;
             default:
                 break;
             }
