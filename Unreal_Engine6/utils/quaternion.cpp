dbd191d57dc845b4ba0b399ea88405d8ff5228a5
mronzon <104356392+mronzon@users.noreply.github.com>
2022-11-13 22:15:16 -0500

    make multiplication between two Quaternions

--- Unreal_Engine6/utils/quaternion.cpp
+++ Unreal_Engine6/utils/quaternion.cpp
@@ -15,3 +15,21 @@ Quaternion::Quaternion(float w, float i, float j, float k)
     _values[2] = j;
     _values[3] = k;
 }
+
+Quaternion::Quaternion(float values[4])
+{
+    _values[0] = values[0];
+    _values[1] = values[1];
+    _values[2] = values[2];
+    _values[3] = values[3];
+}
+
+Quaternion Quaternion::operator*(const Quaternion& other)
+{
+    Quaternion result;
+    result._values[0] = _values[0]*other._values[0] - _values[1]*other._values[1] - _values[2]*other._values[2] - _values[3]*other._values[3];
+    result._values[0] = _values[0]*other._values[1] + _values[1]*other._values[0] + _values[2]*other._values[3] - _values[3]*other._values[2];
+    result._values[0] = _values[0]*other._values[2] + _values[2]*other._values[0] + _values[3]*other._values[1] - _values[1]*other._values[3];
+    result._values[0] = _values[0]*other._values[3] + _values[3]*other._values[0] + _values[1]*other._values[2] - _values[2]*other._values[1];
+    return result;
+}
