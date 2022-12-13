cff0f4ee8399301df5e7953be4d7bfa9f4cc78ae
FERRA <ferralaurianna@gmail.com>
2022-12-12 21:06:05 -0500

    Implémentation de la majorité des fonctions de contact : manque boîte-boîte + nécessite du test

--- Unreal_Engine6/utils/Rigidbodysystem/narrowphasedetector.h
+++ Unreal_Engine6/utils/Rigidbodysystem/narrowphasedetector.h
@@ -19,6 +19,18 @@ private:
     void spherePlane(Actors *act1, Primitives S1, Actors *act2, Primitives P2);
     void boxPlane(Actors *act1, Primitives B1, Actors *act2, Primitives P2);
     void boxSphere(Actors *act1, Primitives B1, Actors *act2, Primitives S2);
+    float clamp(Vector3D centerLocal, Vector3D halfSize, int dimension);
+
+    typedef enum  {
+            VERTEX_FACE = 0,
+            EDGE_FACE_XY = 1,
+            EDGE_FACE_ZY = 2,
+            EDGE_FACE_XZ = 3,
+            FACE_FACE_X = 4,
+            FACE_FACE_Y = 5,
+            FACE_FACE_Z = 6,
+        } TYPE_COLLISION;
+    TYPE_COLLISION whichTypeCollision(Vector3D closePoint,Vector3D halfSize);
 };
 
 #endif // NARROWPHASEDETECTOR_H
